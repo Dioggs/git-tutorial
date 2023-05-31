79cadcf38b25108af372c7e162ac39453444ceac
Diogo Araldo <araldodiogo72@gmail.com>
2023-05-31 11:06:46 -0300

    Criação da funcionalidade básica do gerados de insultos

---
+++ estudoGit.js
@@ -0,0 +1,12 @@
+console.log("BEM VINDO AO GERADOR DE INSULTOS\n");
+
+let insultos = [
+    "vc é feio",
+    "vc é otário",
+    "sua mãe é gorda",
+    "vc é burro",
+    "corno"
+]
+
+insultos.forEach(insulto => console.log(insulto));
+
